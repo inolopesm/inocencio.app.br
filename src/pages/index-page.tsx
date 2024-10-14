804dc52e24a691286011f773efaf01e56e6d4168
Matheus Lopes <inolopesm@gmail.com>
2024-10-14 11:29:30 -0300

    remove favicon componente on home page

--- src/pages/index-page.tsx
+++ src/pages/index-page.tsx
@@ -1,49 +1,38 @@
 import { Car, LinkedinLogo } from "@phosphor-icons/react/dist/ssr";
-import { createPortal } from "react-dom";
 import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
 
 export function Component() {
   return (
-    <>
-      {createPortal(
-        <link
-          href="/assets/images/favicon.svg"
-          rel="icon"
-          type="image/svg+xml"
-        />,
-        window.document.head,
-      )}
-      <div className="bg-gradient-to-tr from-primary/10 to-transparent">
-        <div className="mx-auto flex min-h-dvh max-w-screen-md flex-col items-center">
-          <header className="mx-auto flex h-14 w-full items-center px-4 md:px-6">
-            <Link className="font-serif text-2xl text-primary" to="/">
-              inolopesm
+    <div className="bg-gradient-to-tr from-primary/10 to-transparent">
+      <div className="mx-auto flex min-h-dvh max-w-screen-md flex-col items-center">
+        <header className="mx-auto flex h-14 w-full items-center px-4 md:px-6">
+          <Link className="font-serif text-2xl text-primary" to="/">
+            inolopesm
+          </Link>
+        </header>
+        <main className="flex w-full max-w-[320px] grow flex-col justify-center gap-2 px-4 md:px-6">
+          <Button asChild>
+            <Link to="/auto">
+              <Car className="size-4" />
+              inoauto
             </Link>
-          </header>
-          <main className="flex w-full max-w-[320px] grow flex-col justify-center gap-2">
-            <Button asChild>
-              <Link to="/auto">
-                <Car className="size-4" />
-                inoauto
-              </Link>
-            </Button>
-            <Button variant="secondary" asChild>
-              <a
-                href="https://www.linkedin.com/in/inolopesm/"
-                rel="noreferrer noopener"
-              >
-                <LinkedinLogo className="size-4" />
-                LinkedIn
-              </a>
-            </Button>
-          </main>
-          <footer className="mx-auto flex h-16 items-center px-4 text-center text-sm text-gray-700 md:px-6">
-            Este site é mantido e operado por MATHEUS INOCENCIO LOPES -
-            55.740.093/0001-82
-          </footer>
-        </div>
+          </Button>
+          <Button variant="secondary" asChild>
+            <a
+              href="https://www.linkedin.com/in/inolopesm/"
+              rel="noreferrer noopener"
+            >
+              <LinkedinLogo className="size-4" />
+              LinkedIn
+            </a>
+          </Button>
+        </main>
+        <footer className="mx-auto flex h-16 items-center px-4 text-center text-sm text-gray-700 md:px-6">
+          Este site é mantido e operado por MATHEUS INOCENCIO LOPES -
+          55.740.093/0001-82
+        </footer>
       </div>
-    </>
+    </div>
   );
 }
