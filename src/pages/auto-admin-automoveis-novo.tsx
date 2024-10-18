59c36e559976559f37df5f9657d1df48570d22e6
Matheus Lopes <inolopesm@gmail.com>
2024-10-18 14:43:58 -0300

    fix: allow receive lowercased letters in plate field

--- src/pages/auto-admin-automoveis-novo.tsx
+++ src/pages/auto-admin-automoveis-novo.tsx
@@ -19,13 +19,22 @@ const STATES = [
 ];
 
 // biome-ignore format: better readability
-const LETTERS = [
+const UPPERCASED_LETTERS = [
   "A",    "B",    "C",    "D",    "E",    "F",    "G",
   "H",    "I",    "J",    "K",    "L",    "M",    "N",    "O",
   "P",    "Q",    "R",    "S",    "T",    "U",    "V",    "W",
   "X",    "Y",    "Z",
 ];
 
+// biome-ignore format: better readability
+const LOWERCASED_LETTERS = [
+  "a",    "b",    "c",    "d",    "e",    "f",    "g",
+  "h",    "i",    "j",    "k",    "l",    "m",    "n",    "o",
+  "p",    "q",    "r",    "s",    "t",    "u",    "v",    "w",
+  "x",    "y",    "z",
+];
+
+const LETTERS = [...UPPERCASED_LETTERS, ...LOWERCASED_LETTERS];
 const NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 const PLATE_REGEX = /^[A-Z]{3}[0-9]{1}[0-9A-Z]{1}[0-9]{2}$/;
